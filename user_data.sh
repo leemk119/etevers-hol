c66667757a427e3ad906a79fe8ce6c685d406398
leemk119 <leemk119@gmail.com>
2024-03-28 13:45:02 +0900

    user_data.sh update v3

--- user_data.sh
+++ user_data.sh
@@ -12,7 +12,7 @@ systemctl start httpd
 #Install the web pages for our lab
 if [ ! -f /var/www/html/immersion-day-app-php7.zip ]; then
    cd /var/www/html
-   wget -O 'immersion-day-app-php7.zip' 'https://static.us-east-1.prod.workshops.aws/public/82cf84d3-593a-4fea-adba-6b8f5297abd5/assets/immersion-day-app-php7.zip'
+   wget -O 'immersion-day-app-php7.zip' 'https://etevers-hol.s3.ap-northeast-2.amazonaws.com/Cloud-up-app-php7.zip'
    unzip immersion-day-app-php7.zip
 fi
 
@@ -21,7 +21,7 @@ if [ ! -f /var/www/html/aws.zip ]; then
    cd /var/www/html
    mkdir vendor
    cd vendor
-   wget https://docs.aws.amazon.com/aws-sdk-php/v3/download/aws.zip
+   wget https://etevers-hol.s3.ap-northeast-2.amazonaws.com/aws.zip
    unzip aws.zip
 fi
 
