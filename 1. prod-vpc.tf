efe02c0881df6fcae0e104a168567dea0acbdac8
leemk119 <leemk119@gmail.com>
2024-03-29 12:42:14 +0900

    prod-vpc.tf update v2

--- 1. prod-vpc.tf
+++ 1. prod-vpc.tf
@@ -3,7 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.31.0"
+      version = "~> 5.43.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
     }
   }
 }
