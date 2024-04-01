3ac20914004dc2ff6b8ed82e1d76772efaa5792e
leemk119 <leemk119@gmail.com>
2024-04-01 11:15:27 +0900

    prod-ec2-web.tf update v2

--- 4. prod-ec2-web.tf
+++ 4. prod-ec2-web.tf
@@ -18,14 +18,10 @@
 #   user_data = <<-EOF
 #   #!/bin/bash
 
-#   # Apache 웹 서버 설치
 #   sudo yum update -y
 #   sudo yum install httpd -y
 
-#   # Apache 서버 시작
 #   sudo systemctl start httpd
-
-#   # 부팅 시 자동 시작 설정
 #   sudo systemctl enable httpd
 
 #   echo "Hello, ETEVERS IaC HoL Server-1!" > /var/www/html/index.html
@@ -41,7 +37,7 @@
 #     volume_size           = "30"
 #     volume_type           = "gp3"
 #     encrypted             = false
-#     delete_on_termination = true //EC2 삭제시 함께 제거
+#     delete_on_termination = true
 #   }
 
 #   tags = {
@@ -69,14 +65,10 @@
 #   user_data = <<-EOF
 #   #!/bin/bash
 
-#   # Apache 웹 서버 설치
 #   sudo yum update -y
 #   sudo yum install httpd -y
 
-#   # Apache 서버 시작
 #   sudo systemctl start httpd
-
-#   # 부팅 시 자동 시작 설정
 #   sudo systemctl enable httpd
 
 #   echo "Hello, ETEVERS IaC HoL Server-2!" > /var/www/html/index.html
@@ -92,7 +84,7 @@
 #     volume_size           = "30"
 #     volume_type           = "gp3"
 #     encrypted             = false
-#     delete_on_termination = true //EC2 삭제시 함께 제거
+#     delete_on_termination = true
 #   }
 
 #   tags = {
